f78e7558c0d455acb36394e44b5752a0198958fe
GabrielQuinelato <gabrielpontes1316@gmail.com>
2026-03-18 11:31:51 -0300

    Add files via upload

---
+++ Back/src/test/java/com/senai/pi/vitalux/VitaluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.pi.vitalux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
