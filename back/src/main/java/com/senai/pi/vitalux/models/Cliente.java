ac11c83cac10d7557543153d228c79d1c04373bc
luis-rch0 <luiseduardoalmeida2803@gmail.com>
2026-03-30 11:35:43 -0300

    update commit

--- back/src/main/java/com/senai/pi/vitalux/models/Cliente.java
+++ back/src/main/java/com/senai/pi/vitalux/models/Cliente.java
@@ -1,7 +1,15 @@
+package com.senai.pi.vitalux.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name="endereco")
-public class Endereco {
+@Table(name="cliente")
+public class Cliente {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -19,3 +27,70 @@ public class Endereco {
 
     @Column(name="data_nascimento")
     private String data_nascimento;
+
+    @Column(name="endereco")
+    private String endereco;
+
+    public Cliente(){}
+
+    public Cliente(String email, String nome_completo, String plano, String data_nascimento, String endereco) {
+        this.email = email;
+        this.nome_completo = nome_completo;
+        this.plano = plano;
+        this.data_nascimento = data_nascimento;
+        this.endereco = endereco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome_completo() {
+        return nome_completo;
+    }
+
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
+    }
+
+    public String getPlano() {
+        return plano;
+    }
+
+    public void setPlano(String plano) {
+        this.plano = plano;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
+    
+
+}
+    
