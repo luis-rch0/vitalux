a6036287cdd2a1da670ed6594650955fe57aa5d6
luis-rch0 <luiseduardoalmeida2803@gmail.com>
2026-04-06 20:55:53 -0300

    fix errors

--- back/src/main/java/com/senai/pi/vitalux/models/ListagemMedica.java
+++ back/src/main/java/com/senai/pi/vitalux/models/ListagemMedica.java
@@ -9,8 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="listagem_medica")
 public class ListagemMedica {
     @Id
