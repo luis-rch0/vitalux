c1fea9358d0c3d19ac27ce151275057cd25bf944
luis-rch0 <luiseduardoalmeida2803@gmail.com>
2026-04-06 21:04:54 -0300

    fix

--- back/src/main/java/com/senai/pi/vitalux/models/Cliente.java
+++ back/src/main/java/com/senai/pi/vitalux/models/Cliente.java
@@ -51,5 +51,19 @@ public class Cliente {
 
     @OneToMany(mappedBy = "cliente")
     private List<Farmacia> farmacias;
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
 }
     
