960822e0c76fb7bb05da6fdaff1afc61c046e347
luis-rch0 <luiseduardoalmeida2803@gmail.com>
2026-04-07 09:57:32 -0300

    fix

--- back/src/main/java/com/senai/pi/vitalux/models/Cliente.java
+++ back/src/main/java/com/senai/pi/vitalux/models/Cliente.java
@@ -2,8 +2,6 @@ package com.senai.pi.vitalux.models;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
