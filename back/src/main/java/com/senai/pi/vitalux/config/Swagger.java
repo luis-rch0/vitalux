d9f794f447eadcbd8fc55f2ebd33a81e1494a865
luis-rch0 <luiseduardoalmeida2803@gmail.com>
2026-04-07 10:10:51 -0300

    fix

--- back/src/main/java/com/senai/pi/vitalux/config/Swagger.java
+++ back/src/main/java/com/senai/pi/vitalux/config/Swagger.java
@@ -3,16 +3,15 @@ package com.senai.pi.vitalux.config;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 import io.swagger.v3.oas.annotations.info.Info;
 
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(
-        title = "API",
-        version = "1.0",
-        description = "Gabriel Quinelato, Luis Eduardo. Rafael Felipe, Kayke dos Santos, João Francisco Barbatti"
-    )
-)
+    info = @Info
+    (title = "API",
+    version = "1.0",
+    description = "Gabriel Quinelato, Luis Eduardo. Rafael Felipe, Kayke dos Santos, João Francisco Barbatti"))
 public class Swagger {
 
 }
